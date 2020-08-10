37436a6a542ae20ff16d1505e0aba6ef4dc794b6
Shubham Rana <ranashubham7296@gmail.com>
2020-08-10 17:14:44 +0530

    Added monthly wages for part time and full time employees

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,20 +2,23 @@
 
 echo "Welcome to employee wage computation program on master branch"
 
-attendance=$((RANDOM%1))
+attendance=$((RANDOM%2))
 
 WagePerHr=20
+WorkingDaysPerMonth=20
 
-empType=$((RADNOM%2+1))
+empType=$((RANDOM%2+1))
 
 case $empType in
 	1)
 	EmpHrs=8
 	dailyEmpWage=$(( $WagePerHr * $EmpHrs ))
+	monthlyEmpWage=$(( $dailyEmpWage * $WorkingDaysPerMonth ))
 	;;
 
 	2)
 	EmpHrs=4
 	dailyPartTimeEmpWage=$(( $WagePerHr * $EmpHrs ))
+	monthlyEmpWage=$(( $dailyPartTimeEmpWage * $WorkingDaysPerMonth ))
 	;;
 esac
