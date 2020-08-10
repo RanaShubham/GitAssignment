ca57681d418e99de507a8e8581745be5f35a48cd
Shubham Rana <ranashubham7296@gmail.com>
2020-08-10 22:14:46 +0530

    Calculating daily employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,4 +2,9 @@
 
 echo "Welcome to employee wage computation program on master branch"
 
-attendance=$((RANDOM%1))
+attendance=$((RANDOM%2))
+
+WagePerHr=20
+FullDayHr=8
+
+dailyEmpWage=$(( $WagePerHr * $FullDayHr ))
